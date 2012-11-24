4039a6a8f7bcf6f71d7cc090bfc7a69e5094df0d
Marten Veldthuis <marten@veldthuis.com>
2012-11-24 16:19:14 +0100

    fix tests

--- engines/public/test/functional/public/posts_controller_test.rb
+++ engines/public/test/functional/public/posts_controller_test.rb
@@ -3,14 +3,13 @@ require 'test_helper'
 module Public
   class PostsControllerTest < ActionController::TestCase
     test "should get index" do
-      get :index
+      get :index, use_route: :public
       assert_response :success
     end
   
     test "should get show" do
-      get :show
+      get :show, use_route: :public
       assert_response :success
     end
-  
   end
 end
