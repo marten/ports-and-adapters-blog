b77263ad4ca752df0b0d5fa2a1f6d661a49bbebd
Marten Veldthuis <marten@veldthuis.com>
2012-11-25 16:26:19 +0100

    display posts on site

--- engines/public/app/controllers/public/posts_controller.rb
+++ engines/public/app/controllers/public/posts_controller.rb
@@ -3,6 +3,7 @@ require_dependency "public/application_controller"
 module Public
   class PostsController < ApplicationController
     def index
+      @posts = Public.blog.posts
     end
   
     def show
